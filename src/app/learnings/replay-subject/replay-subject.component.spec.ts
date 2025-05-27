0c0f1e6936de04405833d92e0d51b1fa5ff5194c
Evan BOISSONNOT <evan@boissonnot.fr>
2025-05-27 15:54:41 +0200

    feat: replay + marble

--- src/app/learnings/replay-subject/replay-subject.component.spec.ts
+++ src/app/learnings/replay-subject/replay-subject.component.spec.ts
@@ -1,6 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ReplaySubjectComponent } from './replay-subject.component';
+import { TestScheduler } from 'rxjs/testing';
+import { map, of } from 'rxjs';
+
+const scheduler = new TestScheduler((actual, expected) => {
+  expect(actual).toEqual(expected);
+});
 
 describe('ReplaySubjectComponent', () => {
   let component: ReplaySubjectComponent;
@@ -20,4 +26,17 @@ describe('ReplaySubjectComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should run map', () => {
+    scheduler.run(({cold, expectObservable}) => {
+      const source$ = cold('-a--b--|')
+      const expected = '-x--y--|'
+
+      const results= source$.pipe(
+        map(y => y.toUpperCase())
+      )
+
+      expectObservable(results).toBe(expected, {x: 'A', y: 'B' })
+    })
+  })
 });
